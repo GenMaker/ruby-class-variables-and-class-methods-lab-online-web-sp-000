7eb7f2d372132bcd6073a7222c704c873c6345fc
Panela Leung <leungpanela@gmail.com>
2019-09-01 04:19:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
 
     def self.genres_count
       genres_count = {}
-      @@genres.each do |genres|
+      @@genres.each do |genre|
         if genres_count[genres] # checks to see if hash has the passed in genres
           genres_count[genre] += 1 # adds a new element to the hash
         else # if it is new then add one
