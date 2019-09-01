9eae2bbab07701309ce693593395a4da34729a7d
Panela Leung <leungpanela@gmail.com>
2019-09-01 03:52:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,20 +18,14 @@ class Song
     def genres_count
       genres_count {}
       @@genres.each do |genres|
-        if @@genres != genres
-          genres_count << genres
+        if genres_count[genres] # checks to see if hash has the passed in genres
+          genre_count [genre]+=1 # adds a new element to the hash
+        else # if it is new then add one
+          genre_count[genre]= 1
+        end
+      end
+        genre_count
     end
 
-                            def self.genre_count
-                            genre_count = {}
-                            @@genres.each do |genre|
-                              if genre_count[genre] #if the genre is already in the hash
-                                genre_count[genre] += 1
-                              else #if it's a new genre
-                                genre_count[genre] = 1
-                              end
-                            end
-                            genre_count
-                          end
-
+                          
 end
