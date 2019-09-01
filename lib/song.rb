349a6efdff9474def6793a3b1b2b4118c61ade1f
Panela Leung <leungpanela@gmail.com>
2019-09-01 04:25:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,64 +1,65 @@
 class Song
- attr_accessor :name, :artist, :genre
-
-    @@count = 0
-    @@genres = []
-    @@artist = []
-
-
-    def initialize (name, artist, genre)
-      @@name = name
-      @@artist = [artist]
-      @@genre = genre
-      @@count +=1
-      @@genres << genre
-      @@artist << artist
-    end
-
-    def new (name, artist, genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-    end
-
-    def name
-      @name
-    end
-
-    def artist
-      @artist
-    end
-
-    def genre
-      @genre
-    end
-
-    def self.count
-      album_count = @@album_count.length
-      album_count
-    end
-
-    def self.genres_count
-      genres_count = {}
-      @@genres.each do |genre|
-        if genres_count[genres] # checks to see if hash has the passed in genres
-          genres_count[genre] += 1 # adds a new element to the hash
-        else # if it is new then add one
-          genres_count[genre]= 1
-        end
-      end
-        genres_count
-    end
-
-    def self.artist_count
-      artist_count = {}
-      @@artist.each do |artist|
-        if artist_count |singer| # if singer is in artist_count
-          artist_count[singer] += 1 # add singer's num of songs to to array
-        else
-            artist_count[singer] = 1
-        end
-      end
-      artist_count
-    end
+           # attr_accessor :name, :artist, :genre
+           #
+           #    @@count = 0
+           #    @@genres = []
+           #    @@artist = []
+           #
+           #
+           #    def initialize (name, artist, genre)
+           #      @@name = name
+           #      @@artist = [artist]
+           #      @@genre = genre
+           #      @@count +=1
+           #      @@genres << genre
+           #      @@artist << artist
+           #    end
+           #
+           #    def new (name, artist, genre)
+           #      @name = name
+           #      @artist = artist
+           #      @genre = genre
+           #    end
+           #
+           #    def name
+           #      @name
+           #    end
+           #
+           #    def artist
+           #      @artist
+           #    end
+           #
+           #    def genre
+           #      @genre
+           #    end
+           #
+           #    def self.count
+           #      album_count = @@album_count.length
+           #      album_count
+           #    end
+           #
+           #    def self.genres_count
+           #      genres_count = {}
+           #      @@genres.each do |genre|
+           #        if genres_count[genres] # checks to see if hash has the passed in genres
+           #          genres_count[genre] += 1 # adds a new element to the hash
+           #        else # if it is new then add one
+           #          genres_count[genre]= 1
+           #        end
+           #      end
+           #        genres_count
+           #    end
+           #
+           #    def self.artist_count
+           #      artist_count = {}
+           #      @@artist.each do |artist|
+           #        if artist_count |singer| # if singer is in artist_count
+           #          artist_count[singer] += 1 # add singer's num of songs to to array
+           #        else
+           #            artist_count[singer] = 1
+           #        end
+           #      end
+           #      artist_count
+           #    end
+    
 end
