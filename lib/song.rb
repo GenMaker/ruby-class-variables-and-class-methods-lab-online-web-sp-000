51f98d7ed749014061516dbfcb4bc8617d843a79
Panela Leung <leungpanela@gmail.com>
2019-09-01 03:34:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
 class Song
  attr_accessor :name, :artist, :genre
 
-@@count = 0
+    @@count = 0
 
-def initialize
-  @@count +=1
-end
+    def initialize
+      @@count +=1
+    end
 
-def self.count
-  @@album_count
-end
+    def self.count
+      @@album_count
+    end
 
-def genres (genres)
-  @genres = genres
-  if @genres.include?(genres) == false
-      @genres[genres] = []
-  end
-      @genres[genres] << genres
+    def genres (genres)
+      @genres = genres
+      if @genres.include?(genres) == false
+          @genres[genres] = []
+      end
+          @genres[genres] << genres
 end
